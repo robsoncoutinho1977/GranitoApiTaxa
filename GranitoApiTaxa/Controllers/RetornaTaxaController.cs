b257aec138f4b55a1c2500cfd1687b883df8c49e
Robson de Oliveira <rob.coutinho@gmail.com>
2023-03-29 23:12:43 -0300

    Add project files.

---
+++ GranitoApiTaxa/Controllers/RetornaTaxaController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GranitoApiTaxa.Controllers
+{
+    [Route("granito/[controller]")]
+    [ApiController]
+    public class RetornaTaxaController : ControllerBase
+    {
+        [HttpGet]
+        public int Get()
+        {
+            return 1;
+        }
+    }
+}
